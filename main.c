eadc2a2737f729290f8b5771fc7d7ebc1174ff89
Kazuki Okamoto <kakkun61@gmail.com>
2011-09-30 02:44:58 +0900

    コンパイルエラー時の処理の変更

--- main.c
+++ main.c
@@ -8,6 +8,7 @@
 
 int main(int argc, char **argv) {
 	FILE *input;
+	int compile_error;
 
 	if (argc == 1) input = stdin;
 	else if (argc == 2) {
@@ -17,18 +18,23 @@ int main(int argc, char **argv) {
 			return 1;
 		}
 	} else {
-		fprintf(stderr, "Bad call.\n");
+		fprintf(stderr, "Usage:\n");
+		fprintf(stderr, "\t%s [source file]\n", argv[0]);
 	}
 
 	set_compile_script(create_script());
 
-	compile(input);
+	compile_error = compile(input);
 
 	if (input != stdin) {
 		if (EOF == fclose(input)) {
-			fprintf(stderr, "Fail to close file.\n");
+			fprintf(stderr, "Fail to close \"%s\".\n", argv[1]);
 			return 1;
 		}
+	}
+
+	if (compile_error) {
+		return 1;
 	}
 
 	interpret(get_compile_script());

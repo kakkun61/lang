068202e9872edadec83eb80c7a016a802e88e2af
Kazuki Okamoto <kakkun61@gmail.com>
2011-11-18 19:21:40 +0900

    ソース移動

---
+++ src/builtin.c
@@ -0,0 +1,14 @@
+#include "builtin.h"
+#include "lang.h"
+#include <stdio.h>
+
+Value *lang_puts(Context *const context, ValueList *const argument_list) {
+	ValueList *vl;
+	char str[80];
+	FOR (vl, argument_list, next) {
+		value2string(str, 80, vl->value);
+		printf("%s\n", str);
+	}
+	return create_value(NULL_VALUE);
+}
+
---
+++ src/builtin.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "eval.h"
+
+Value *lang_puts(Context *const context, ValueList *const argument_list);
+
